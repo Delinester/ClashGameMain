6ff00f022117982ee688b5add2e0804bcd5aabcc
Ilchikaev Amir <ilchikaev@bk.ru>
2024-12-21 13:39:34 +0500

    Spawning world map character and camera controller change

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -16,6 +17,9 @@ public class CameraController : MonoBehaviour
 
     private Vector3 mousePosition;
     private GameObject playerToFollow;
+    private GameRole currentRole;
+
+    private bool isFollowingPlayer = true;
     void Awake()
     {
         if (instance == null)
@@ -33,6 +37,7 @@ public class CameraController : MonoBehaviour
     void Start()
     {        
         playerToFollow = LocalStateManager.instance.localPlayerCharacter;
+        currentRole = LocalStateManager.instance.localPlayer.GetComponent<PlayerNetworking>().synchronizedPlayerGameData.role;
         // Set camera position to the location according to player's role!
         //Vector3 townPos = GameManager.instance.GetTownTeamPosition(LocalStateManager.instance.localPlayer.GetComponent<PlayerNetworking>().synchronizedPlayerGameData.teamNumber);
         //m_camera.transform.position = new Vector3(townPos.x, townPos.y, m_camera.transform.position.z);
@@ -42,6 +47,13 @@ public class CameraController : MonoBehaviour
     { 
         currentBoundaries = bounds; 
     }
+
+    public void SetFollowingPlayer(bool isFollowing)
+    {
+        isFollowingPlayer = isFollowing;
+    }
+
+    
 
     // Update is called once per frame
     void Update()
@@ -60,8 +72,26 @@ public class CameraController : MonoBehaviour
             float bottom = cameraPosition.y - height / 2;
 
             //Debug.Log($"Left: {left}, Right: {right}, Top: {top}, Bottom: {bottom}" + " BOUNDS " + currentBoundaries.min + " max " + currentBoundaries.max);
+            Vector3 newPos = transform.position;
+            if (!isFollowingPlayer)
+            {
+                if (Input.GetMouseButtonDown(1))
+                {
+                    mousePosition = Input.mousePosition;
+                }
 
-            Vector3 newPos = new Vector3(playerToFollow.transform.position.x, playerToFollow.transform.position.y, m_camera.transform.position.z);
+                if (Input.GetKey(KeyCode.Mouse1))
+                {
+                    Vector3 currentMousePosition = Input.mousePosition;
+                    Vector3 delta = currentMousePosition - mousePosition;
+                    float x = m_camera.transform.position.x - delta.x * moveSpeed * Time.deltaTime;
+                    float y = m_camera.transform.position.y - delta.y * moveSpeed * Time.deltaTime;
+                    newPos = new Vector3(x, y, m_camera.transform.position.z);
+                }
+            }
+            else
+                newPos = new Vector3(playerToFollow.transform.position.x, playerToFollow.transform.position.y, m_camera.transform.position.z);
+
             if (currentBoundaries.min.x != 0)
             {
                 //if (left < currentBoundaries.min.x) newPos.x = currentBoundaries.min.x;
@@ -79,7 +109,7 @@ public class CameraController : MonoBehaviour
             if (mouseScrolDelta < 0f) ortoSize += zoomSpeed * Time.deltaTime;
             else if (mouseScrolDelta > 0f) ortoSize -= zoomSpeed * Time.deltaTime;
 
-            if (ortoSize > 5f) ortoSize = 5;
+            if (ortoSize > 4f) ortoSize = 4;
             else if (ortoSize < 3f) ortoSize = 3;
 
             m_camera.orthographicSize = ortoSize;
